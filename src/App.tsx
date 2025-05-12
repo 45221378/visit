05a556d8c27b30a11344771d707e2a4b12c94921
梅雨露(Miya Mei/米娅) <miya@aopacloud.sg>
2025-05-12 19:40:14 +0800

    feat: 更新的Aim啊

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Modal from '/common/components/Modal/Modal'
 import { GlobalContextProvider } from './context/globalContext'
 import ola from './ola'
 import MyAudio from './pages/Audio/Audio'
+import './font.scss'
 
 const env = {
   lan: ola.user.lan,
@@ -21,6 +22,11 @@ export default function App() {
       <GlobalContextProvider>
         <MyAudio />
         <Router />
+        <div className='bottom-info'>
+          <p className='p1'>蒂升电梯(中国)所有@2021</p>
+          <div className='p1'>沪ICP备17004808号-9A</div>
+          <p className='p12'>沪公网安备 31010402009937号</p>
+        </div>
       </GlobalContextProvider>
     </ErrorBoundary>
   )
